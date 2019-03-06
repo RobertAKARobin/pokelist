03d29b0ea18e4bf87839a9c61493af4f25e74547
RobertAKARobin <robertgfthomas@gmail.com>
2019-03-06 08:50:13 -0600

    Added types and readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pokélist
+
+For teaching Gulp/SASS/Github Pages.
+
+---
+
+<details><summary>What's the most repetive part of [./dist/main.css](./dist/main.css)?</summary>
+All the classes for Pokémon types.
+</details>
+
+---
+
+<details><summary>In [./dist/main.css](./dist/main.css), which lines occur more than once?
+
+Don't count blank lines and `}`. Hint: there are 6.</summary>
+
+2.	2 x `@media (min-width: 500px) {`
+3.	2 x `@media (max-width: 499px) {`
+1.	3 x `#pokemon-list {`
+4.	3 x `#pokemon-list div {`
+5.	5 x `color: #fff;`
+6.	10 x `color: #000;`
+</details>
+
+---
--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,17 @@
 $(document).ready(async ()=>{
-	const $output = $('#output')
-	const pokemonCollection = await $.getJSON('./pokemon.json')
+	const $pokemonList = $('#pokemon-list')
+	const $pokemonTypes = $('#pokemon-types')
+	const pokemonList = await $.getJSON('./pokemon.json')
+	const pokemonTypes = Object.keys(pokemonList.reduce((types, pokemon)=>{
+		types[pokemon.type] = 1
+		return types
+	}, {})).sort()
 
-	$output.append(pokemonCollection.map(pokemon=>{
-		return `<div class="pokemon ${pokemon.type.toLowerCase()}">${pokemon.id}. ${pokemon.name}<br />${pokemon.type}</div>`
+	$pokemonList.append(pokemonList.map(pokemon=>{
+		return `<div class="type-${pokemon.type.toLowerCase()}"><h2>${pokemon.id}. ${pokemon.name}</h2>${pokemon.type}</div>`
+	}))
+
+	$pokemonTypes.append(pokemonTypes.map(type=>{
+		return `<div class="type-${type.toLowerCase()}">${type}</div>`
 	}))
 })
